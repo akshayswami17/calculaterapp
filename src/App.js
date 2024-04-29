341168573a495ab693cebcc11b1e4939a2d08122
akshayswami17 <akshayswami1731@gmail.com>
2024-04-29 16:50:21 +0530

    Added App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
-import './App.css';
-import ButtonsContainer from './Components/ButtonsContainer';
+import Display from "./Components/Display";
+import ButtonsContainer from "./Components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } 
+    else if (buttonText === "Ak") {
+      setCalVal("Welcome.");
+      
+      
+    } 
+    else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
   return (
-    <div>
-      <h1>Calculater</h1>
-    <ButtonsContainer></ButtonsContainer>
+    <div className={styles.calculator}>
+      <h1 className={styles.h1}>Calculator</h1>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
     </div>
   );
 }
